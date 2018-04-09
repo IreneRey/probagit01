a0326f55ec6dece47e932fb7225b34a86d222cdd
IreneRey <a17irenerr@iessanclemente.net>
2018-04-09 12:17:33 +0200

    Cambio cores

--- OlaMundo.java
+++ OlaMundo.java
@@ -1,6 +1,9 @@
 public class OlaMundo{
   public static void main (String[] args){
-  System.out.println("Ola Mundo");
-  System.out.println("Ola Mundo version basica");
+	String laranxa = "\033[33m";
+	String azul="\033[34m";
+
+  System.out.println(laranxa + "Ola Mundo");
+  System.out.println(azul + "Ola Mundo version basica");
   }
 }
